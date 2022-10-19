84e348682dd687faeea4917bcfc319916d919e43
kelmane <etai.kelman@gmail.com>
2022-10-19 12:36:26 +0300

    Update multijob.groovy

--- basics/multijob.groovy
+++ basics/multijob.groovy
@@ -1,22 +1,30 @@
-job('NodeJS example1') {
-
+job('EK example1') {
+    scm {
+        git('https://github.com/kelmane/docker-cicd.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('DSL yaniv')
+            node / gitConfigEmail('blahblah@bla.bla')
+        }
+    }
     triggers {
         scm('H/5 * * * *')
     }
-   
     steps {
-        shell("npm install")
+        shell("docker build -t test:test .")
     }
 }
 
-job('NodeJS example2') {
-
+job('EK example2') {
+    scm {
+        git('https://github.com/kelmane/docker-cicd.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('DSL yaniv')
+            node / gitConfigEmail('blahblah@bla.bla')
+        }
+    }
     triggers {
         scm('H/5 * * * *')
     }
-    
     steps {
-        shell("npm install")
+        shell("docker build -t test:test .")
     }
 }
 
