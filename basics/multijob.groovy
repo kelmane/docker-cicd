fd4c2c501a300dc00a5e78ac50f4f7e5abf3930f
kelmane <etai.kelman@gmail.com>
2022-10-19 13:55:13 +0300

    Update multijob.groovy

--- basics/multijob.groovy
+++ basics/multijob.groovy
@@ -1,15 +1,19 @@
-job('EK example1') {
+job('NodeJS example') {
     scm {
-        git('https://github.com/kelmane/docker-cicd.git') {  node -> // is hudson.plugins.git.GitSCM
+        git('http://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL yaniv')
-            node / gitConfigEmail('blahblah@bla.bla')
+            node / gitConfigEmail('jenkins-dsl@newtech.academy')
         }
     }
     triggers {
         scm('H/5 * * * *')
     }
+    wrappers {
+        nodejs('nodejs') // this is the name of the NodeJS installation in 
+                         // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
+    }
     steps {
-        shell("docker build -t test:test .")
+        shell("npm install")
     }
 }
 
